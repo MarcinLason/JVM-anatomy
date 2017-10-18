b7df26ca5bc3ff6f93bcabfe764c0b62e3236943
Marcin Lason <marcin.lason1@gmail.com>
2017-10-18 12:31:29 +0200

    Add Test class and template for editing Test.class

--- Lab1/Marcin_Lason/zadanie1-1/classes/Test.java
+++ Lab1/Marcin_Lason/zadanie1-1/classes/Test.java
@@ -3,7 +3,25 @@ import java.io.IOException;
 public class Test {
     private int someInt;
 
+    private int getSomeInt () {
+        someInt = 30;
+        return someInt;
+    }
+
+    private void returnNothing () {
+        int number = 30;
+        String text = "text";
+    }
+
+    private double getSomeDouble() {
+        return someInt + 0.5;
+    }
+
+
     public static void main(String[] argv) throws IOException {
+        Test test = new Test();
         System.out.println("Hello world");
+        System.out.println(test.getSomeInt());
+        System.out.println(test.getSomeDouble());
     }
 }
--- Lab1/Marcin_Lason/zadanie1-1/src/Transform.java
+++ Lab1/Marcin_Lason/zadanie1-1/src/Transform.java
@@ -1,4 +1,51 @@
+import org.apache.bcel.Repository;
+import org.apache.bcel.classfile.JavaClass;
+import org.apache.bcel.classfile.Method;
+import org.apache.bcel.generic.ClassGen;
+import org.apache.bcel.generic.ConstantPoolGen;
+import org.apache.bcel.generic.MethodGen;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 public class Transform {
+
     public static void main(String[] args) {
+        String testClassName = "";
+        JavaClass testClass = null;
+
+        if (args.length > 0) {
+            testClassName = args[0].replace(".class", "");
+        }
+
+        try {
+            testClass = Repository.lookupClass(testClassName);
+        } catch (ClassNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+
+        if (testClass == null) return;
+        List<Method> methods = Arrays.asList(testClass.getMethods());
+
+
+
+        ClassGen testClassModification = new ClassGen(testClass);
+        ConstantPoolGen poolGen = testClassModification.getConstantPool();
+
+        for (Method m : methods) {
+            MethodGen method = new MethodGen(m, testClassModification.getClassName(), poolGen);
+
+
+        }
+
+
+
+        String classFilePath = Repository.lookupClassFile(testClassModification.getClassName()).getPath();
+        try {
+            testClassModification.getJavaClass().dump(classFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
