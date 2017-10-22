a2785fe82ee4a35e35b8a18d7d33f99956f18c50
Marcin Lason <marcin.lason1@gmail.com>
2017-10-22 22:18:15 +0200

    Implement exercise 1.1

--- Lab1/Marcin_Lason/zadanie1-1/src/Transform.java
+++ Lab1/Marcin_Lason/zadanie1-1/src/Transform.java
@@ -1,60 +1,126 @@
+import org.apache.bcel.Constants;
 import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
 import org.apache.bcel.generic.*;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.io.PrintStream;
 
 public class Transform {
+    private static final String PREFIX = "Method to be called: ";
+    private static final String RESULT = "Got result: ";
 
     public static void main(String[] args) {
-        String testClassName = "";
-        JavaClass testClass = null;
-
-        if (args.length > 0) {
-            testClassName = args[0].replace(".class", "");
+        JavaClass testClass = getTestClass(args);
+        if (testClass == null) {
+            return;
         }
+        ClassGen modifiedClass = modifyTestClass(testClass);
+        saveModifiedClass(modifiedClass);
+    }
 
-        try {
-            testClass = Repository.lookupClass(testClassName);
-        } catch (ClassNotFoundException e) {
-            System.err.println(e.getMessage());
-        }
+    private static ClassGen modifyTestClass(JavaClass originalClass) {
+        ClassGen modifiedClass = new ClassGen(originalClass);
+        InstructionFactory instructionFactory = new InstructionFactory(modifiedClass);
+        ConstantPoolGen constantPool = modifiedClass.getConstantPool();
 
-        if (testClass == null) return;
-        List<Method> methods = Arrays.asList(testClass.getMethods());
+        for (Method method : originalClass.getMethods()) {
+            MethodGen modifiedMethod = new MethodGen(method, method.getName(), constantPool);
+            InstructionList instructionList = modifiedMethod.getInstructionList();
 
-
-        ClassGen testClassModification = new ClassGen(testClass);
-        ConstantPoolGen constantPoolGen = testClassModification.getConstantPool();
-        InstructionFactory instructionFactory = new InstructionFactory(testClassModification);
-
-        for (Method method : methods) {
-            MethodGen methodModification = new MethodGen(method, testClassModification.getClassName(), constantPoolGen);
-            System.out.println("\nMethod's name: " + method.getName().toString());
-            System.out.println("Return type: " + method.getReturnType().toString());
-
-            InstructionList instructionList = methodModification.getInstructionList();
-            InstructionHandle[] instructionHandles = instructionList.getInstructionHandles();
-
-            for (InstructionHandle handle : instructionHandles) {
+            for (InstructionHandle handle : instructionList.getInstructionHandles()) {
                 if (handle.getInstruction() instanceof InvokeInstruction) {
-                    if (!(handle.getInstruction() instanceof INVOKESPECIAL)) {
-                        System.out.println(handle.getInstruction().getName());
+                    InvokeInstruction instruction = (InvokeInstruction) handle.getInstruction();
+                    Type instructionReturnType = instruction.getReturnType(constantPool);
+
+                    if (instructionReturnType != Type.VOID) {
+                        String methodMessage = prepareMessage(instruction, constantPool);
+                        instructionList.insert(handle, instructionFactory.createPrintln(methodMessage));
+
+                        if (!(instruction instanceof INVOKESPECIAL)) {
+                            instructionList.append(handle, buildInstructionList(instructionFactory, instructionReturnType));
+                        }
                     }
                 }
             }
+        }
+        return modifiedClass;
+    }
 
+    private static InstructionList buildInstructionList(InstructionFactory factory, Type returnType) {
+        InstructionList modifiedInstructionList = new InstructionList();
+
+        modifiedInstructionList.append(getSystemInstruction(factory));
+        modifiedInstructionList.append(factory.createConstant(RESULT));
+        modifiedInstructionList.append(getPrintInstruction(factory));
+        modifiedInstructionList.append(InstructionFactory.createDup(returnType.getSize()));
+        modifiedInstructionList.append(getSystemInstruction(factory));
+
+        if (returnType.equals(Type.LONG) || returnType.equals(Type.DOUBLE)) {
+            modifiedInstructionList.append(InstructionFactory.DUP_X2);
+            modifiedInstructionList.append(InstructionFactory.POP);
+        } else {
+            modifiedInstructionList.append(InstructionFactory.SWAP);
         }
 
+        modifiedInstructionList.append(getPrintlnInstruction(factory, checkReturnedType(returnType)));
+        return modifiedInstructionList;
+    }
 
-        String classFilePath = Repository.lookupClassFile(testClassModification.getClassName()).getPath();
+    private static String getTestClassName(String[] args) {
+        String testClassName = "";
+        if (args.length > 0) {
+            testClassName = args[0].replace(".class", "");
+        }
+        return testClassName;
+    }
+
+    private static JavaClass getTestClass(String[] args) {
+        JavaClass testClass = null;
         try {
-            testClassModification.getJavaClass().dump(classFilePath);
+            testClass = Repository.lookupClass(getTestClassName(args));
+        } catch (ClassNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+        return testClass;
+    }
+
+    private static void saveModifiedClass(ClassGen modifiedClass) {
+        String path = Repository.lookupClassFile(modifiedClass.getClassName()).getPath();
+        try {
+            modifiedClass.getJavaClass().dump(path);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static Type checkReturnedType(Type returnType) {
+        if (returnType.getSignature().contains("/") || returnType.getSignature().contains("[")) {
+            return new ObjectType("java.lang.Object");
+        }
+        return returnType;
+    }
+
+    private static String prepareMessage(InvokeInstruction instruction, ConstantPoolGen cp) {
+        return PREFIX + instruction.getMethodName(cp) + instruction.getSignature(cp);
+    }
+
+    private static Instruction getSystemInstruction(InstructionFactory factory) {
+        return factory.createFieldAccess(
+                System.class.getCanonicalName(), "out", new ObjectType(PrintStream.class.getCanonicalName()),
+                Constants.GETSTATIC);
+    }
+
+    private static Instruction getPrintInstruction(InstructionFactory factory) {
+        return factory.createInvoke(
+                PrintStream.class.getCanonicalName(), "print", Type.VOID, new Type[]{new ObjectType("java.lang.String")},
+                Constants.INVOKEVIRTUAL);
+    }
+
+    private static Instruction getPrintlnInstruction(InstructionFactory factory, Type type) {
+        return factory.createInvoke(
+                PrintStream.class.getCanonicalName(),"println", Type.VOID, new Type[]{type},
+                Constants.INVOKEVIRTUAL);
     }
 }
