6d51e6489dbe27155bdafb8bbb8618ca758feb8b
Marcin Lason <marcin.lason1@gmail.com>
2017-11-11 14:21:14 +0100

    Lab3 - Add tests

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd Lab3
+
+for i in $( ls ); do
+ cd $i
+ cd zadanie3
+ ant &> ../compile.txt
+
+ CLASSPATH=.
+ cd lib
+
+ for j in $( ls *.jar ); do
+   export CLASSPATH=$CLASSPATH\;../lib/$j
+ done
+
+ cd ../classes
+ java -Dnpj.heap.size=21 Interpreter test.npj &> test_out.txt
+ cd ..
+done
